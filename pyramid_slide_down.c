125c1483654307682eeaefbe3ae51f2d0a5a1e1f
Mikhailichenko Dmitry <hhrshh@gmail.com>
2024-08-08 19:38:51 +0300

    Pyramid slide down

---
+++ pyramid_slide_down.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <ctype.h>
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+// Memory model of `pyramid`:
+// `pyramid` is a one-dimensional array containing the elements of a pyramid with `rows` levels,
+// in direct succession from left to right, top to bottom
+int pyramid_slide_down(int num_elements, const int pyramid[num_elements], int rows)
+{
+    // TODO
+    return -1;
+}
+
+
+int main(void)
+{
+    
+    return 0;
+}

9f96f8ef16cf3adf378426e17ff66966fdae23b4
Mikhailichenko Dmitry <hhrshh@gmail.com>
2024-08-16 11:40:52 +0300

    Count ip addresses

--- count_ip_addresses.c
+++ count_ip_addresses.c
@@ -1,11 +1,11 @@
-#include <stddef.h>
+#include <inttypes.h>
+#include <inttypes.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <ctype.h>
 
+#define IP2UINT32(i1,i2,i3,i4)  i1<<24|i2<<16|i3<<8|i4
 // Implement a function that receives two IPv4 addresses, and returns the number of addresses between them
 // (including the first one, excluding the last one).
 
@@ -16,17 +16,24 @@
 // * With input "10.0.0.0", "10.0.1.0"   => return  256 
 // * With input "20.0.0.10", "20.0.1.0"  => return  246
 
-uint32_t ips_between(const char *start, const char *end)
+uint32_t ips_between (const char *start, const char *end)
 {
-  return 0;
-}
+  uint32_t is[4], ie[4], s, e;
 
+  sscanf(start, "%d.%d.%d.%d", &is[0], &is[1], &is[2], &is[3]);
+  sscanf(end, "%d.%d.%d.%d", &ie[0], &ie[1], &ie[2], &ie[3]);
+
+  s = IP2UINT32(is[0], is[1], is[2], is[3]);
+  e = IP2UINT32(ie[0], ie[1], ie[2], ie[3]);
+  
+  return e - s;
+}
 
 int main(void)
 {
-    char start[] = "10.0.0.0";
-    char end[] = "10.0.0.50";
+    char start[] = "0.0.0.0";
+    char end[] = "255.255.255.255";
+    printf("%ld\n", ips_between(start, end));
 
-    
     return 0;
 }

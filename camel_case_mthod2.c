bc52e37ecd08b77bb93d074bda0fb48b9aeb8dc8
Mikhailichenko Dmitry <hhrshh@gmail.com>
2024-08-28 12:38:44 +0300

    Camel case method

--- camel_case_mthod2.c
+++ camel_case_mthod2.c
@@ -12,24 +12,27 @@
 
 char *camel_case(const char *s)
 {
+    int num = strlen(s);
     char *p = NULL;
-    p = (char*)malloc(sizeof(char) * strlen(s) + 1);
-
-    int i = 0, j = 0, flag = 0;
-    p[j++] = s[i] & 223; //Upper register
-    while(s[++i])
+    p = (char*)malloc(sizeof(char) * num + 1);
+    if(p == NULL)
+       printf("Memory allocated error");
+    int j = 0, flag = 1;
+    for(int i = 0; i < num; ++i)
+    {
         if(isalpha(s[i]))
         {
+            p[j++] = s[i];
             if(flag)
             {
-                p[j++] = s[i] ^ 32;
+                p[j - 1] = toupper(p[j - 1]);
                 flag = 0;
             }
-            else
-                p[j++] = s[i];
         }
         else
             flag = 1;
+    }
+    p[j] = '\0';
     return p;
 }
 
@@ -37,7 +40,7 @@ char *camel_case(const char *s)
 
 int main(void)
 {
-    char* s = "The sunset sets at twelve**o!clock!!"; 
+    char* s = "hello case"; 
     printf("%s\n", camel_case(s));
     return 0;
 }
